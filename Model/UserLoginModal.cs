5fac50920f5edee1850ff3f191987dd44e72ef6f
cubil <cubillasant@gmail.com>
2026-02-03 18:05:36 -0700

    Add project files.

---
+++ Model/UserLoginModal.cs
@@ -0,0 +1,14 @@
+﻿namespace CapstoneAPI.Model
+{
+    public class UserLoginModal
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
+
